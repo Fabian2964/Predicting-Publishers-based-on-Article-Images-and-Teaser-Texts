b37e193a4356455a025b8ecb9646207d847820b7
Fabian2964 <fabianpaul1@gmx.de>
2022-08-06 01:46:09 +0200

    submit

--- app_image.py
+++ app_image.py
@@ -13,8 +13,6 @@ from tensorflow.keras import preprocessing
 from tensorflow.keras.models import load_model
 from tensorflow.keras.activations import softmax
 from tensorflow.keras.preprocessing import image as IMG
-import os
-import h5py
 
 st.header("News Image Class Predictor - Frankfurter Allgemeine vs. Spiegel")
 
@@ -31,13 +29,17 @@ if file_uploaded is not None:
 	img = IMG.load_img(file_uploaded,target_size=[150,150])
 
 
-classifier = tf.keras.models.load_model(r'vgg16_model_array_fazsp.h5')
-x = IMG.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-class_names = ['FAZ', 'Spiegel']
-pred = classification.predict(x)
-scores = tf.nn.softmax(pred[0])
-scores = scores.numpy()
-image_class = class_names[np.argmax(scores)]
-result = "The image uploaded is: {}".format(image_class)
-return result
+	classifier = tf.keras.models.load_model(r'vgg16_model_array_fazsp.h5')
+	x = IMG.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	class_names = ['FAZ', 'Spiegel']
+	pred = classification.predict(x)
+	scores = tf.nn.softmax(pred[0])
+	scores = scores.numpy()
+	image_class = class_names[np.argmax(scores)]
+	result = "The image uploaded is: {}".format(image_class)
+	result
+
+ERROR: pip's dependency resolver does not currently take into account all the packages that are installed. This behaviour is the source of the following dependency conflicts.
+
+tensorflow 2.9.1 requires protobuf<3.20,>=3.9.2, but you have protobuf 3.20.1 which is incompatible.
