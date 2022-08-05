b48dd710599bac0f97797ff1863761c4735d2b48
Fabian2964 <fabianpaul1@gmx.de>
2022-08-06 01:39:29 +0200

    submit

--- app_image.py
+++ app_image.py
@@ -18,29 +18,26 @@ import h5py
 
 st.header("News Image Class Predictor - Frankfurter Allgemeine vs. Spiegel")
 
-def main():
-	file_uploaded = st.file_uploader("Choose the file", type = ['jpg', 'png', 'jpeg'])
-	if file_uploaded is not None:
-		image = Image.open(file_uploaded)
-		figure = plt.figure()
-		plt.imshow(image)
-		plt.axis('off')
-		result = predict_class(image)
-		st.write(result)
-		st.pyplot(figure)
-		img = IMG.load_img(file_uploaded,target_size=[150,150])
 
-def predict_class(image):
-	classifier = tf.keras.models.load_model(r'vgg16_model_array_fazsp.h5')
-	x = IMG.img_to_array(img)
-	x = np.expand_dims(x, axis=0)
-	class_names = ['FAZ', 'Spiegel']
-	pred = classification.predict(x)
-	scores = tf.nn.softmax(pred[0])
-	scores = scores.numpy()
-	image_class = class_names[np.argmax(scores)]
-	result = "The image uploaded is: {}".format(image_class)
-	return result
-	
-if __name__ == "__main__":
-	main()
+file_uploaded = st.file_uploader("Choose the file", type = ['jpg', 'png', 'jpeg'])
+if file_uploaded is not None:
+	image = Image.open(file_uploaded)
+	figure = plt.figure()
+	plt.imshow(image)
+	plt.axis('off')
+	result = predict_class(image)
+	st.write(result)
+	st.pyplot(figure)
+	img = IMG.load_img(file_uploaded,target_size=[150,150])
+
+
+classifier = tf.keras.models.load_model(r'vgg16_model_array_fazsp.h5')
+x = IMG.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+class_names = ['FAZ', 'Spiegel']
+pred = classification.predict(x)
+scores = tf.nn.softmax(pred[0])
+scores = scores.numpy()
+image_class = class_names[np.argmax(scores)]
+result = "The image uploaded is: {}".format(image_class)
+return result
